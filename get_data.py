afdaabf216739fa6f1ffc8a681eb4db0db1fd56c
vamsi-krishna-2005 <vutlavamsikrishna@gmail.com>
2026-04-03 22:04:36 +0530

    Added Data version 2

--- get_data.py
+++ get_data.py
@@ -8,8 +8,8 @@ os.makedirs('data', exist_ok=True)
 california = fetch_california_housing(as_frame=True)
 df = california.frame
 
-# VERSION 1: We only save the first 10,000 rows
-df_v1 = df.head(10000)
-df_v1.to_csv('data/housing.csv', index=False)
+# VERSION 2: We saved full data
+df_v2 = df
+df_v2.to_csv('data/housing.csv', index=False)
 
-print(f"Version 1 saved with {len(df_v1)} rows.")
+print(f"Version 2 saved with {len(df_v2)} rows.")
